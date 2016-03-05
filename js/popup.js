21caab0fe572e42f638c0910ec6e185b3fbee392
Jonathan Guillotte-Blouin <jonathan.guillotte.blouin@gmail.com>
2016-03-05 10:21:34 -0500

    basic function call, need to put into closures

--- js/popup.js
+++ js/popup.js
@@ -1,9 +1,23 @@
+function add() {
+	alert('hello add');
+}
+
+function run() {
+	alert('runnnning');
+}
+
+function view() {
+	alert('this view');
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   var lis = document.querySelectorAll('li');
 
   for (var i = 0; i < lis.length; i++) {
   	lis[i].addEventListener('click', function() {
-  		alert(this.id);
+  		window[this.id] === undefined ?
+  			window[this.id]() :
+  			alert('not existing');
   	});
   }
 }, false);
